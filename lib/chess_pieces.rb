619b60e3de1550e2bfd4157d64813e1e8f32633f
soryko <sorykora@gmail.com>
2024-05-27 14:13:07 +0200

    created knight class

--- lib/chess_pieces.rb
+++ lib/chess_pieces.rb
@@ -15,3 +15,19 @@ class Pawn < Piece
   end
 
 end
+
+class Rook < Piece
+  attr_reader :moves
+
+  def initialize
+    moves = { horizontal:(8,0), vertical:(0,8)}
+  end
+end
+
+class Knight < Piece
+  attr_reader :moves
+
+  def initialize
+    moves = { southW:(1,-2), southE:(-1,-2), northW:(1,2), northE:(-1,2), westN:(2,1), westS:(2,-1), eastN:(-2,1), eastS:(-2,-1) }
+  end
+end
