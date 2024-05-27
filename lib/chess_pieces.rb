033a84ac0a5a241a60d4eb849d0147a3b88f5f9d
soryko <sorykora@gmail.com>
2024-05-27 14:40:20 +0200

    initalized all chess pieces

--- lib/chess_pieces.rb
+++ lib/chess_pieces.rb
@@ -11,7 +11,7 @@ class Pawn < Piece
   attr_reader :moves
 
   def initialize
-    moves =  { up:(0,1), upup:(0,2), capture_left:(-1,1), capture_right:(1,1) }
+    @moves =  { up:(0,1), upup:(0,2), capture_left:(-1,1), capture_right:(1,1) }
   end
 
 end
@@ -20,7 +20,7 @@ class Rook < Piece
   attr_reader :moves
 
   def initialize
-    moves = { horizontal:(8,0), vertical:(0,8)}
+    @moves = { horizontal:(8,0), vertical:(0,8)}
   end
 end
 
@@ -28,6 +28,56 @@ class Knight < Piece
   attr_reader :moves
 
   def initialize
-    moves = { southW:(1,-2), southE:(-1,-2), northW:(1,2), northE:(-1,2), westN:(2,1), westS:(2,-1), eastN:(-2,1), eastS:(-2,-1) }
+    @moves = { southW:(1,-2), southE:(-1,-2), northW:(1,2), northE:(-1,2), westN:(2,1), westS:(2,-1), eastN:(-2,1), eastS:(-2,-1) }
+  end
+end
+
+class Bishop < Piece
+  attr_reader :moves
+
+  def initalize
+    @moves = { updiagonal: @updiagonal, downdiagonal: @downdiagonal}
+    @updiagonal = updiagonal()
+    @downdiagonal = downdiagonal()
+  end
+
+  def updiagonal
+    coordinates = []
+    (-8..8).each do |x|
+      (-8..8).each do |y|
+        coordinates << (x,y)
+      end
+    end
+    coordinates
+  end
+
+  def downdiagonal
+    coordinates = []
+    (-8..8).each do |x|
+      (8..-8).each do |y|
+        coordinates << (x,y)
+      end
+    end
+    coordinates
+  end
+end
+
+class king
+  attr_reader :moves
+
+  def initalize
+    @moves = { north:(0,1), northE:(-1,1), northW:(1,1), east:(-1,0), west:(1,0), south:(0,-1), southE:(-1,-1), southW:(-1,1)}
+  end
+end
+
+class queen
+  attr_reader :moves
+
+  def initalize
+    @moves = { updiagonal:@updiagonal, downdiagonal:@downdiagonal, horizontal:@horizontal, vertical:@vertical}
+    @updiagonal = Bishop.updiagonal
+    @downdiagonal = Bishop.downdiagonal
+    @horizontal = Rook.horizontal
+    @vertical = Rook.vertical
   end
 end
